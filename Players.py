1b7213e2ae10fc3cdb5e91b78e76158e52220705
Benjamin de Boissieu <bendeb@hotmail.com>
2019-02-23 16:39:15 +0100

    Add files via upload

--- Players.py
+++ Players.py
@@ -32,3 +32,28 @@ class Player:
     def has_weapon(self):
         if self.weapon is not None:
             return self.weapon
+
+class Warrior(Player):
+    def __init__(self,pseudo,health,attack):
+        #Pour recuperer les elements du parents
+        super().__init__(pseudo, health, attack)
+        # ajout d'une amure pour le warrior
+        self.armor = 3
+        print('Bienvenue au guerrier ', pseudo, 'health ', health, 'attack ', attack, 'weapon ', self.weapon)
+
+    def damage(self,damage):
+        if self.armor > 0:
+            self.armor -= 1
+            damage = 0
+        #pour recuper la fonction damage parent
+        super().damage(damage)
+        print('Aie', self.pseudo,  '.... Vous venez de subir ', damage, 'dégats')
+
+    # pour recharger l armure
+    def blade(self):
+        self.armor = 3
+        print('Les  points d armure ont été rechargés')
+
+    # pour connaitre combien de points armure le warrior a
+    def get_points_armor(self):
+        return self.armor
